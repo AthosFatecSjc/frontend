7889bc1d98fe39a76de604fbcaec5895715a6364
Pablo <pablogregorio8@gmail.com>
2026-03-30 21:03:09 -0300

    ATS-28 feat: up inicial da tela de login + rota

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts"></script>
 
-
-
 <template>
-  <nav style="margin-bottom: 1rem">
-    <router-link to="/indicadores">Indicadores</router-link>
-  </nav>
   <router-view />
 </template>
